2f9c71ae742aebff5c51963aace13eb7e5227811
vthang95 <tranvietthang95@gmail.com>
2017-03-08 22:45:40 +0700

    collide world bounds

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const GAME_MIN_WIDTH = 320;
 const GAME_MIN_HEIGHT = 480;
 const GAME_MAX_WIDTH = 640;
 const GAME_MAX_HEIGHT = 960;
+const SPACESHIP_SIZE_WIDTH = 78;
+const SPACESHIP_SIZE_HEIGHT = 78;
 
 window.onload = function() {
     Nakama.game = new Phaser.Game(GAME_MAX_WIDTH, GAME_MAX_HEIGHT, Phaser.AUTO, '', {
@@ -31,29 +33,34 @@ var preload = function() {
     Nakama.game.load.image('background', 'Assets/Map1.png');
 }
 
-const SPACESHIP_1_X = 200;
-const SPACESHIP_2_Y = 200;
-
 // initialize the game
 var create = function() {
+    const SPACESHIP_1_SPAWN_CORDINATE_X = 200;
+    const SPACESHIP_1_SPAWN_CORDINATE_Y = 200;
     Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
     Nakama.keyboard = Nakama.game.input.keyboard;
 
     Nakama.game.add.sprite(0, 0, 'background');
-    Nakama.player = Nakama.game.add.sprite(SPACESHIP_1_X, SPACESHIP_2_Y, 'assets', 'Spaceship1-Player.png');
-    // Nakama.player.body.collideWorldBounds = true;
+    Nakama.player = Nakama.game.add.sprite(
+        SPACESHIP_1_SPAWN_CORDINATE_X,
+        SPACESHIP_1_SPAWN_CORDINATE_Y,
+        'assets',
+        'Spaceship1-Player.png'
+    );
 }
 
 // update game state each frame
 var update = function() {
-    if (Nakama.keyboard.isDown(Phaser.Keyboard.UP)) {
+    if (Nakama.keyboard.isDown(Phaser.Keyboard.UP) && Nakama.player.position.y > 0) {
         Nakama.player.position.y -= 10;
-    } else if (Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+    } else if (Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)
+                && Nakama.player.position.y < GAME_MAX_HEIGHT - SPACESHIP_SIZE_HEIGHT) {
         Nakama.player.position.y += 10;
     }
-    if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT) && Nakama.player.position.x > 0) {
         Nakama.player.position.x -= 10;
-    } else if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+    } else if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)
+                && Nakama.player.position.x < GAME_MAX_WIDTH - SPACESHIP_SIZE_WIDTH) {
         Nakama.player.position.x += 10;
     }
 }
